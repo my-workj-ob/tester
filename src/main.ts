df1f452971bd281eac33cadde4c27aab3e368ea6
baxtiyorWeb <baxtiyorqurbonnazarov33@gmail.com>
2025-04-02 13:35:30 +0500

    fix #10

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,43 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NestFactory } from '@nestjs/core';
-import { Express } from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
+import { join } from 'path';
 import { AppModule } from './app.module';
-
-export default async (): Promise<Express> => {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.init();
-  return app.getHttpAdapter().getInstance() as Express;
-};
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (
+        !origin ||
+        [
+          'https://it-experts-one.vercel.app',
+          'http://localhost:3000',
+          'http://localhost:3030',
+        ].includes(origin)
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // 🔥 BU MUHIM!
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth API')
+    .setDescription('NestJS Authentication API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+  await app.listen(process.env.PORT || 3000);
+  //
+}
+bootstrap();
