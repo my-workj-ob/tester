92bc6b2088a3a655eab6757065a296a33a411bff
baxtiyorWeb <baxtiyorqurbonnazarov33@gmail.com>
2025-04-03 21:02:20 +0500

    ok

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NestFactory } from '@nestjs/core';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -5,18 +9,38 @@ import cors from 'cors';
 import express from 'express';
 import { join } from 'path';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 
+  app.use((req, res, next) => {
+    res.header(
+      'Access-Control-Allow-Origin',
+      'https://it-experts-nine.vercel.app',
+    );
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS',
+    );
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+
+    next();
+  });
+
+  const server = express();
+  server.use(cors());
+  app.useWebSocketAdapter(new IoAdapter(app));
   app.use(
     cors({
       origin: 'https://it-experts-nine.vercel.app',
       credentials: true,
     }),
   );
-  app.useWebSocketAdapter(new IoAdapter(app));
 
   const config = new DocumentBuilder()
     .setTitle('Auth API')
@@ -33,4 +57,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT || 3000);
 }
+bootstrap();
 bootstrap();
